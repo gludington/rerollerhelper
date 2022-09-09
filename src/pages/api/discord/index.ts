8a1001e9831d7b19ae6df8c5a377d8ca17cba0a0
Greg Ludington <Greg.Ludington@ipsoft.com>
2022-09-09 01:49:54 -0400

    adding early discord bot attempts

---
+++ src/pages/api/discord/index.ts
@@ -0,0 +1,73 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { InteractionResponseType, InteractionType, verifyKey } from "discord-interactions";
+import getRawBody from "raw-body";
+import { env } from "../../../env/server.mjs";
+
+const INVITE_COMMAND = {
+  name: "Invite",
+  description: "Get an invite link to add the bot to your server",
+};
+
+const HI_COMMAND = {
+  name: "Hi",
+  description: "Say hello!",
+};
+
+const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${env["APPLICATION_ID"]}&scope=applications.commands`;
+
+/**
+ * Gotta see someone 'bout a trout
+ * @param {VercelRequest} request
+ * @param {VercelResponse} response
+ */
+module.exports = async (request: VercelRequest, response: VercelResponse) => {
+  console.error(request);
+  if (request.method === "GET") {
+    console.error(INVITE_URL);
+  }
+  // Only respond to POST requests
+  if (request.method === "POST") {
+    // Verify the request
+    const signature = request.headers["x-signature-ed25519"];
+    const timestamp = request.headers["x-signature-timestamp"];
+    const rawBody = await getRawBody(request);
+
+    const isValidRequest = verifyKey(rawBody, signature, timestamp, env["PUBLIC_KEY"]);
+
+    if (!isValidRequest) {
+      return response.status(401).send({ error: "Bad request signature " });
+    }
+
+    // Handle the request
+    const message = request.body;
+
+    // Handle PINGs from Discord
+    if (message.type === InteractionType.PING) {
+      console.log("Handling Ping request");
+      response.send({
+        type: InteractionResponseType.PONG,
+      });
+    } else if (message.type === InteractionType.APPLICATION_COMMAND) {
+      // Handle our Slash Commands
+      switch (message.data.name.toLowerCase()) {
+        case INVITE_COMMAND.name.toLowerCase():
+          response.status(200).send({
+            type: 4,
+            data: {
+              content: INVITE_URL,
+              flags: 64,
+            },
+          });
+          console.log("Invite request");
+          break;
+        default:
+          console.error("Unknown Command");
+          response.status(400).send({ error: "Unknown Type" });
+          break;
+      }
+    } else {
+      console.error("Unknown Type");
+      response.status(400).send({ error: "Unknown Type" });
+    }
+  }
+};
