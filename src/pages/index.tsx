1323ad818b3f7ec3a4f67ed77c2427dc3af6491f
Greg Ludington <Greg.Ludington@ipsoft.com>
2022-09-08 21:13:43 -0400

    simple discord auth with nextauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -17,6 +18,14 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h1>
+        {session ? (
+          <>
+          <h3>Hello {session.user.name}</h3>
+          <button onClick={() => signOut()}>Logout</button>
+          </>
+        ): (
+              <button onClick={() => signIn("discord")}>Login with Discord</button>
+        )}
         <p className="text-2xl text-gray-700">This stack uses:</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
           <TechnologyCard
